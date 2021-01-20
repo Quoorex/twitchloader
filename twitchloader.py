d28719f6c2f8aaf62eb8b8fd4c767c93606e4da3
Quoorex <46283604+Quoorex@users.noreply.github.com>
2021-01-20 21:30:34 +0100

    Update the dependencies

--- twitchloader.py
+++ twitchloader.py
@@ -13,14 +13,18 @@ from fuzzywuzzy import process
 import youtube_dl
 
 
-__version__ = '0.1'
-__url__ = 'https://github.com/Quoorex/twitchloader'
+__version__ = "0.1"
+__url__ = "https://github.com/Quoorex/twitchloader"
 
 
 # TODO Add threading or multiprocessing
+# (Twitch API has a ratelimit though,
+# but the API is only used to gather the links in the beginning anyway)
+
+# TODO Fix rename randomly creating folders with parts of video titles as their name
 
 
-class Twitchloader():
+class Twitchloader:
     def __init__(self):
         self.conf = self.init_parser()
         client_id = self.conf.client_id
@@ -28,37 +32,114 @@ class Twitchloader():
 
         self.video_url_base = "https://www.twitch.tv/videos/"
         self.api_base = "https://api.twitch.tv/kraken/"
-        self.headers = {"Accept": "application/vnd.twitchtv.v5+json", "Client-ID": client_id}
+        self.headers = {
+            "Accept": "application/vnd.twitchtv.v5+json",
+            "Client-ID": client_id,
+        }
 
     def init_parser(self):
         ydl_options_default = {
             "format": "best",
-            "outtmpl": '%(download_dir)s/%(uploader)s/%(collection_name)s/%(video_index)s - %(title)s.%(ext)s',
-            "urls_outtmpl": '%(download_dir)s/%(uploader)s/%(title)s.%(ext)s',
+            "outtmpl": "%(download_dir)s/%(uploader)s/%(collection_name)s/%(video_index)s - %(title)s.%(ext)s",
+            "urls_outtmpl": "%(download_dir)s/%(uploader)s/%(title)s.%(ext)s",
         }
         self.parser = configargparse.ArgParser(
             config_file_parser_class=configargparse.YAMLConfigFileParser,
             default_config_files=["config.yaml"],
-            description='Download VODs and complete video collections from Twitch.tv using youtube-dl.'
+            description="Download VODs and complete video collections from Twitch.tv using youtube-dl.",
         )
-        self.parser.add_argument("-c", "--config-path", is_config_file=True, dest="config-path", help="path to the config file")
-        self.parser.add_argument("-C", "--channels", dest="channels", nargs="+", type=yaml.safe_load, help="names of the channels to get the collections of")
-        self.parser.add_argument("-t", "--client-id", dest="client_id", help="Twitch client ID needed to access the API (get one on https://dev.twitch.tv/)")
-        self.parser.add_argument("--collection-ids", dest="collection_ids", type=yaml.safe_load, nargs="+", help="ids of the collections to process")
-        self.parser.add_argument("--show-collections", action="store_true", dest="show_collections", help="show the collections of the channels")
-        self.parser.add_argument("--save-urls", action="store_true", dest="save_urls", help="save the urls of the videos in separated folders instead of downloading them (for manual use with the '-a' youtube-dl option")
-        self.parser.add_argument("-o", "--output-dir", dest="output_dir", default="downloads", type=str, help="Path to where the files will be saved")
-        self.parser.add_argument("-y", "--ydl-options", dest="ydl_options", type=yaml.safe_load, default=ydl_options_default, help="Youtube-DL options (https://github.com/ytdl-org/youtube-dl/blob/3e4cedf9e8cd3157df2457df7274d0c842421945/youtube_dl/YoutubeDL.py#L137-L312)")
-        self.parser.add_argument("-u", "--urls", dest="urls", type=yaml.safe_load, nargs="+", help="URLs of the videos to download")
-        self.parser.add_argument("--rename-existing", dest="rename_existing", type=bool, help="Rename exsting files")
-        self.parser.add_argument("--rename-outtmpl", dest="rename_outtmpl", type=str, help="Youtube-DL compatible output template for renaming files")
-        self.parser.add_argument("--match-ratio", dest="match_ratio", type=str, help="Percent ratio of how similar the name of an existing file has to be to be considered for the rename")
+        self.parser.add_argument(
+            "-c",
+            "--config-path",
+            is_config_file=True,
+            dest="config-path",
+            help="path to the config file",
+        )
+        self.parser.add_argument(
+            "-C",
+            "--channels",
+            dest="channels",
+            nargs="+",
+            type=yaml.safe_load,
+            help="names of the channels to get the collections of",
+        )
+        self.parser.add_argument(
+            "-t",
+            "--client-id",
+            dest="client_id",
+            help="Twitch client ID needed to access the API (get one on https://dev.twitch.tv/)",
+        )
+        self.parser.add_argument(
+            "--collection-ids",
+            dest="collection_ids",
+            type=yaml.safe_load,
+            nargs="+",
+            help="ids of the collections to process",
+        )
+        self.parser.add_argument(
+            "--show-collections",
+            action="store_true",
+            dest="show_collections",
+            help="show the collections of the channels",
+        )
+        self.parser.add_argument(
+            "--save-urls",
+            action="store_true",
+            dest="save_urls",
+            help="save the urls of the videos in separated folders instead of downloading them (for manual use with the '-a' youtube-dl option",
+        )
+        self.parser.add_argument(
+            "-o",
+            "--output-dir",
+            dest="output_dir",
+            default="downloads",
+            type=str,
+            help="Path to where the files will be saved",
+        )
+        self.parser.add_argument(
+            "-y",
+            "--ydl-options",
+            dest="ydl_options",
+            type=yaml.safe_load,
+            default=ydl_options_default,
+            help="Youtube-DL options (https://github.com/ytdl-org/youtube-dl/blob/3e4cedf9e8cd3157df2457df7274d0c842421945/youtube_dl/YoutubeDL.py#L137-L312)",
+        )
+        self.parser.add_argument(
+            "-u",
+            "--urls",
+            dest="urls",
+            type=yaml.safe_load,
+            nargs="+",
+            help="URLs of the videos to download",
+        )
+        self.parser.add_argument(
+            "--rename-existing",
+            dest="rename_existing",
+            type=bool,
+            help="Rename exsting files",
+        )
+        self.parser.add_argument(
+            "--rename-outtmpl",
+            dest="rename_outtmpl",
+            type=str,
+            help="Youtube-DL compatible output template for renaming files",
+        )
+        self.parser.add_argument(
+            "--match-ratio",
+            dest="match_ratio",
+            type=str,
+            help="Percent ratio of how similar the name of an existing file has to be to be considered for the rename",
+        )
         return self.parser.parse()
 
     def print_banner(self):
         colorama.init()
         figlet = Figlet(font="speed")
-        print(colorama.Fore.MAGENTA + colorama.Back.BLACK + figlet.renderText("Twitchloader"))
+        print(
+            colorama.Fore.MAGENTA
+            + colorama.Back.BLACK
+            + figlet.renderText("Twitchloader")
+        )
         print(colorama.Back.RESET + "Download Twitch videos with ease")
         print(colorama.Style.RESET_ALL)
 
@@ -77,13 +158,19 @@ class Twitchloader():
         params = {"limit": 100}
         request_url = self.api_base + f"channels/{channel_id}/collections"
 
-        collections_response = requests.get(request_url, headers=self.headers, params=params).json()
+        collections_response = requests.get(
+            request_url, headers=self.headers, params=params
+        ).json()
         collections = collections_response["collections"]
 
-        if collections_response["_cursor"] is not None:  # Not all collections could be fetched with one request
+        if (
+            collections_response["_cursor"] is not None
+        ):  # Not all collections could be fetched with one request
             params["cursor"] = collections_response["_cursor"]
             while params["cursor"] is not None:
-                r = requests.get(request_url, headers=self.headers, params=params).json()
+                r = requests.get(
+                    request_url, headers=self.headers, params=params
+                ).json()
                 collections.extend(r["collections"])
                 params["cursor"] = r["_cursor"]
 
@@ -94,11 +181,13 @@ class Twitchloader():
         video_count = 0
 
         for collection in collections:
-            videos = self.client.collections.get(collection["_id"], include_all_items=True)
+            videos = self.client.collections.get(
+                collection["_id"], include_all_items=True
+            )
             video_urls = []
 
             for video in videos:
-                video_url = self.video_url_base + video['item_id']
+                video_url = self.video_url_base + video["item_id"]
                 video_urls.append(video_url)
             collections_dict[collection["_id"]] = [collection, video_urls]
             video_count += collection["items_count"]
@@ -116,7 +205,9 @@ class Twitchloader():
             uploader = collection_item["owner"]["name"]
 
             # Declare the download path and create it if necessary
-            download_path = os.path.abspath(f"{download_dir}/{uploader}/{collection_name}/")
+            download_path = os.path.abspath(
+                f"{download_dir}/{uploader}/{collection_name}/"
+            )
             pathlib.Path(download_path).mkdir(parents=True, exist_ok=True)
 
             with open(os.path.join(download_path, "urls.txt"), "w") as f:
@@ -127,15 +218,22 @@ class Twitchloader():
         """
         Replaces some parts of the outtmpl, that are specific to this program and youtube-dl therefore isn't able to handle
         """
-        return outtmpl.replace("%(download_dir)s", download_dir).replace("%(collection_name)s", collection_name).replace("%(video_index)s", str(video_index))
+        return (
+            outtmpl.replace("%(download_dir)s", download_dir)
+            .replace("%(collection_name)s", collection_name)
+            .replace("%(video_index)s", str(video_index))
+        )
 
     def rename_existing(self, video_url, download_dir, collection_name, video_index):
         outtmpl = self.conf.rename_outtmpl
         with youtube_dl.YoutubeDL(self.conf.ydl_options) as ydl:
             info_dict = ydl.extract_info(video_url, download=False)
-        filepath = self.process_outtmpl(outtmpl, download_dir, collection_name, video_index) % info_dict
+        filepath = (
+            self.process_outtmpl(outtmpl, download_dir, collection_name, video_index)
+            % info_dict
+        )
         path, filename = os.path.split(filepath)
-        if not(os.path.exists(path)):
+        if not (os.path.exists(path)):
             os.makedirs(path)
         match = process.extractOne(filename, os.listdir(path))
         if not match:
@@ -161,10 +259,19 @@ class Twitchloader():
             collection_name = collection_item["title"]
 
             for video_url in video_urls:
-                video_index = video_urls.index(video_url) + 1  # Add 1 because lists start at 0
-                ydl_options["outtmpl"] = self.process_outtmpl(original_outtmpl, download_dir, collection_name, video_index)
+                video_index = (
+                    video_urls.index(video_url) + 1
+                )  # Add 1 because lists start at 0
+                ydl_options["outtmpl"] = self.process_outtmpl(
+                    original_outtmpl, download_dir, collection_name, video_index
+                )
                 if self.conf.rename_existing is True:
-                    if self.rename_existing(video_url, download_dir, collection_name, video_index) is True:
+                    if (
+                        self.rename_existing(
+                            video_url, download_dir, collection_name, video_index
+                        )
+                        is True
+                    ):
                         continue  # File exists and was renamed; programm can continue with the next video
                 with youtube_dl.YoutubeDL(ydl_options) as ydl:
                     try:
@@ -181,7 +288,9 @@ class Twitchloader():
         self.print_figlet("standard", "Starting the video downloads")
 
         download_dir = self.conf.output_dir
-        ydl_options["outtmpl"] = ydl_options["urls_outtmpl"].replace("%(download_dir)s", download_dir)
+        ydl_options["outtmpl"] = ydl_options["urls_outtmpl"].replace(
+            "%(download_dir)s", download_dir
+        )
 
         for video_url in video_urls:
             if self.conf.rename_existing is True:
